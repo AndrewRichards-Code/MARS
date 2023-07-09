29d1b02b9e2bb4da5662f3a0ef3fb5b8a1977322
Andrew Richards <andyjrichards93@gmail.com>
2023-07-09 14:25:46 +0100

    Updated Perspective and Orthographic matrices for reverse z and right handed styles.

--- src/Matrix/Matrix4.h
+++ src/Matrix/Matrix4.h
@@ -110,24 +110,66 @@ namespace mars
 		}
 
 		//Constructs a orthographic matrix (Matrix4).
-		//For Normalised Device Co-ordinates of X: -1 to 1, Y: -1 to 1 and Z: 0 to 1 in a Left-Handed system.
-		static Matrix4 Orthographic(float left, float right, float bottom, float top, float _near, float _far)
+		//For Normalised Device Co-ordinates of X: -1 to 1, Y: -1 to 1 and Z: 0 to 1 in a Left-Handed system.  Options for Reverse Z and Right-Handed system.
+		//https://github.com/microsoft/DirectXMath/blob/main/Inc/DirectXMathMatrix.inl //Handed-ness and Reverse Z.
+		static Matrix4 Orthographic(float left, float right, float bottom, float top, float zNear, float zFar, bool reverseZ = false, bool rightHanded = false)
 		{
+			if (reverseZ)
+			{
+				const float temp = zNear;
+				zNear = zFar;
+				zFar = temp;
+			}
+
+			T A = 0, B = 0, C = 0, X = 0, Y = 0, Z = 0;
+			A = static_cast<T>(2) / static_cast<T>(right - left);
+			B = static_cast<T>(2) / static_cast<T>(top - bottom);
+			X = static_cast<T>((right + left) / (right - left));
+			Y = static_cast<T>((top + bottom) / (top - bottom));
+			if (rightHanded) 
+			{
+				C = static_cast<T>(1) / static_cast<T>(zNear - zFar);
+				Z = static_cast<T>(zNear) * C;
+			}
+			else
+			{
+				C = static_cast<T>(1) / static_cast<T>(zFar - zNear);
+				Z = static_cast<T>(-zNear) * C;
+			}
 			return Matrix4(
-				(2 / (static_cast<T>(right - left))),	(0),								(0),								(static_cast<T>(-(right + left) / (right - left))),
-				(0),									(2 / static_cast<T>(top - bottom)),	(0),								(static_cast<T>(-(top + bottom) / (top - bottom))),
-				(0),									(0),								(1 / static_cast<T>(_far - _near)),	(static_cast<T>(-(_near) / (_far - _near))),
-				(0),									(0),								(0),								(1));
+				A, 0, 0, X,
+				0, B, 0, Y,
+				0, 0, C, Z,
+				0, 0, 0, 1);
 		}
+
 		//Constructs a perspective matrix (Matrix4). Input fov is in radians.
-		//For Normalised Device Co-ordinates of X: -1 to 1, Y: -1 to 1 and Z: 0 to 1 in a Left-Handed system.
-		static Matrix4 Perspective(double fov, float aspectRatio, float _near, float _far)
+		//For Normalised Device Co-ordinates of X: -1 to 1, Y: -1 to 1 and Z: 0 to 1 in a Left-Handed system. Options for Reverse Z and Right-Handed system.
+		//https://www.gamedev.net/tutorials/programming/graphics/perspective-projections-in-lh-and-rh-systems-r3598/ //Handed-ness.
+		//https://github.com/sebbbi/rust_test/commit/d64119ce22a6a4972e97b8566e3bbd221123fcbb //Reverse Z.
+		static Matrix4 Perspective(double fov, float aspectRatio, float zNear, float zFar, bool reverseZ = false, bool rightHanded = false)
 		{
+
+			T A = 0, B = 0, C = 0, D = 0, E = 0;
+			A = static_cast<T>(1) / static_cast<T>(aspectRatio * static_cast<float>(tan(fov / 2.0)));
+			B = static_cast<T>(1) / static_cast<T>(static_cast<float>(tan(fov / 2)));
+			D = rightHanded ? static_cast<T>(-1) : static_cast<T>(1);
+			if (reverseZ)
+			{
+				C = static_cast<T>((-zNear) / (zFar - zNear));
+				E = (rightHanded ? static_cast<T>(zFar) : static_cast<T>(-zFar)) * C;
+			}
+			else
+			{
+				C = static_cast<T>((zFar) / (zFar - zNear));
+				E = (rightHanded ? static_cast<T>(zNear) : static_cast<T>(-zNear)) * C;
+			}
+
 			return Matrix4(
-				(1 / static_cast<T>(aspectRatio * static_cast<float>(tan(fov / 2)))),	(0),														(0),										(0),
-				(0),																	(1 / static_cast<T>(static_cast<float>(tan(fov / 2)))),		(0),										(0),
-				(0),																	(0),														(static_cast<T>((_far) / (_far - _near))),	(static_cast<T>(-(_far * _near) / (_far - _near))),
-				(0),																	(0),														(1),										(0));
+				A, 0, 0, 0,
+				0, B, 0, 0,
+				0, 0, C, E,
+				0, 0, D, 0);
 		}
 
 		//Constructs a translation matrix.
